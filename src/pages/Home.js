53b6663c15f89dc2f5447b9896732a9385c94a82
Brian-José Mejia-Rivera <e1746533@cmaisonneuve.qc.ca>
2021-06-28 14:12:23 -0400

    Working api!

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,7 +6,8 @@ import '../css/home.css';
 class Home extends React.Component {
 
     state = {
-        countries: [],
+        world_data: [],
+        world_total_cases: [],
         loading: true
     }
 
@@ -16,32 +17,60 @@ class Home extends React.Component {
             duration : 500
           });
 
+          /*
         //const url = "https://api.covid19api.com/countries";
         const url = "https://api.covid19api.com/world/total";
         const response = await fetch(url);
         const data = await response.json();
         this.setState({ world_total_data: data, loading: false });
+        */
+
+        //"https://covid-193.p.rapidapi.com/statistics"
+
+
+
+        fetch("https://api.quarantine.country/api/v1/summary/latest")
+        .then(response => response.json())
+        .then(data => this.setState({world_total_cases:data.data, loading: false }))
+        .catch(err => {
+            console.error(err);
+        });
     }
 
     render() {
-        if (this.state.loading)
+        if (this.state.loading) {
             return <div>Loading...</div>;
+        }
+        else{
 
-        return (
-            <div id="home-body">
-                <ul data-aos="fade-up">
-                    <li className="data-block">
-                        Total Confirmed: {this.state.world_total_data.TotalConfirmed}
-                    </li>
-                    <li>
-                        Total Recovered: {this.state.world_total_data.TotalRecovered}
-                    </li>
-                    <li>
-                        Total Deaths: {this.state.world_total_data.TotalDeaths}
-                    </li>
-                </ul>
-            </div>
-        );
+            console.log(this.state.world_total_cases.summary)
+
+
+            /*
+            this.state.world_data.forEach(country => {
+                this.state.world_total_cases.totalConfirmed += country.cases.total;
+            });
+            */
+
+            return (
+                <div id="home-body">
+                    <ul data-aos="fade-up">
+                        <li className="data-block total">
+                            Total Confirmed: {this.state.world_total_cases.summary.total_cases.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ")}
+                        </li>
+                        <li className="data-block total-confirmed">
+                            Total Active Cases: {this.state.world_total_cases.summary.active_cases.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ")}
+                        </li>
+                        <li className="data-block total-recovered">
+                            Total Recovered: {this.state.world_total_cases.summary.recovered.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ")}
+                        </li>
+                        <li className="data-block total-deaths">
+                            Total Deaths: {this.state.world_total_cases.summary.deaths.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ")}
+                        </li>
+                    </ul>
+                </div>
+            );
+        }
     }
 }
 
